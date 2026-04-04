4349950701bddd7cdac1e34a8b69ac90db54a4de
kritika-2006 <kritika02056@gmail.com>
2026-04-04 21:06:30 +0530

    main

---
+++ calculator.py
@@ -0,0 +1,9 @@
+a = 2
+b = 3
+
+print ("The value of",a, "+",b, "is:",a+b)
+print ("The value of",a, "-",b, "is:",a-b)
+print ("The value of",a, "*",b, "is:",a*b)
+print ("The value of",a, "/",b, "is:",a/b)
+print ("The value of",a, "//",b, "is:",a//b)
+print ("The value of",a, "**",b, "is:",a**b)
