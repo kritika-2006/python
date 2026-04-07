55f05a0cbec6d18b3ae4e140e93abfffcc5fa0c4
kritika-2006 <kritika02056@gmail.com>
2026-04-07 21:20:33 +0530

    main

---
+++ elif.py
@@ -0,0 +1,8 @@
+applePrice = 20
+budget = 100
+if( budget - applePrice > 50):
+    print("add 1 kg apples to the cart.")
+elif(budget - applePrice > 70):
+    print("Its okay you can buy")
+else:
+    print("Do not add Apples to the cart")
