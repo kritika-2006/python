f68ee2605113128cc5e6a470e78318c73af0b4f9
kritika-2006 <kritika02056@gmail.com>
2026-04-07 21:15:06 +0530

    main

---
+++ if - else.py
@@ -0,0 +1,15 @@
+a = int(input("Enter your age:"))
+print("Your age is:",a)
+
+# conditional operators >,<,<=,==,!=
+print(a>18)
+print(a<18)
+print(a<=18)
+print(a==18)
+print(a!=18)
+
+if(a>18):
+    print("You can vote")
+else:
+    print("You cannot vote")
+print ("Underage")
