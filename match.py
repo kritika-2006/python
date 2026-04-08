56ec166b6598e56c40ffe9d57d0c34751c4a5cb3
kritika-2006 <kritika02056@gmail.com>
2026-04-08 22:35:14 +0530

    main

---
+++ match.py
@@ -0,0 +1,22 @@
+a = int(input("Enter the value of a: "))
+
+match a:
+    case 0:
+        print("a is zero")
+
+    case 4:
+        print("case is 4")
+
+    case _ if a!=90:
+        print(a, "is not 90")
+
+    case _ if a!=80:
+        print(a,"is not 80")
+    case _:
+        print(x)
+
+
+
+
+
+
