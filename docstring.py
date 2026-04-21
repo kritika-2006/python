75813dd38deb96622c1648e74f6dde80ea7d78e3
kritika-2006 <kritika02056@gmail.com>
2026-04-21 18:54:55 +0530

    main

---
+++ docstring.py
@@ -0,0 +1,6 @@
+def square(n):
+    '''Takes in a number n, returns the square of n'''
+    return n**2
+
+print(square(5))       # This will print 25
+print(square.__doc__)  # This will print the docstring
