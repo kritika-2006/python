36c8784b4fbbed427899dab3144408aba2793de0
kritika-2006 <kritika02056@gmail.com>
2026-04-22 21:39:11 +0530

    main

---
+++ set.py
@@ -0,0 +1,14 @@
+# in the set elements are not repeated
+# set dosen't maintain order
+s = {2,3,7,9,4,2}
+print(s)
+# in the set we use the forin loop
+for value in s:
+    print(s)
+    
+kritika = { } # empty dictionary
+print(type(kritika))
+
+# empty set
+k = set()
+print(type(k))
