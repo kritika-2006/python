8320cf4bcd6ea6a169bcde2a52f1aa739e51a906
kritika-2006 <kritika02056@gmail.com>
2026-04-25 20:40:24 +0530

    main

---
+++ for with else.py
@@ -0,0 +1,9 @@
+for i in range(5):
+    print(i)
+    if(i==4):
+     break
+else:
+    print("Sorry no i")
+    # in for loop with else 
+    # else statement does not print due to break statement
+
---
+++ while with else.py
@@ -0,0 +1,10 @@
+# while loop with else 
+# same statement
+i = 0 
+while i<7:
+   print(i)
+   i = i + 1
+   if i == 4:
+      break
+   else:
+      print("Say no to i")
