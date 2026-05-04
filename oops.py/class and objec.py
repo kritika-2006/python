ea43d27711990ed7d14fce8b7eb0b1d71de173b2
kritika-2006 <kritika02056@gmail.com>
2026-05-04 19:36:43 +0530

    main

---
+++ oops.py/class and objec.py
@@ -0,0 +1,22 @@
+# class
+class Person:
+    name = "kritika"
+    occupation = "Cyber security"
+    networth = 10
+
+    def info(self):
+        # self is used to access the current instance of the class
+        print(f"{self.name} is a {self.occupation}")
+
+a = Person()
+b = Person()
+# object
+a.name = "ritika"
+a.occupation = "Software Engineer"
+
+b.name = "kirti"
+b.occupation = "HR"
+
+a.info()
+b.info()
+
