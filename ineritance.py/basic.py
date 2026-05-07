69d770004a957edf2db51057028398833b6b9bb5
kritika-2006 <kritika02056@gmail.com>
2026-05-07 20:42:46 +0530

    main

---
+++ ineritance.py/basic.py
@@ -0,0 +1,13 @@
+class Employee:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
+    def showDetails(self):
+        print(f"The name of Employee : {self.id} is {self.name}")
+
+e1 = Employee("Kritika", 500)
+e1.showDetails() 
+
+e2 = Employee("Kriti", "320")
+e2.showDetails()
