12cafaa3f115a4d7b76c60e835da5874453eebe0
kritika-2006 <kritika02056@gmail.com>
2026-06-20 10:30:57 +0530

    main

---
+++ exe12.py
@@ -0,0 +1,10 @@
+bus_details = {
+    "route" :"sonipat to college",
+      "bus_number":"44",
+      "passengers": ["Anjali", "manvi", "rahul"] 
+}
+print(bus_details)
+bus_details["bus_number"] = "45"
+bus_details["passengers"].append("kritika")
+print(bus_details)
+
