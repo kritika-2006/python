48948559a9ce7f500eb509b66557b199a9a1f6a4
kritika-2006 <kritika02056@gmail.com>
2026-06-21 17:07:12 +0530

    main

---
+++ exe14.py
@@ -0,0 +1,8 @@
+def add_bus_passenger(passenger_name):
+    return f"{passenger_name} is sit in the bus"
+
+passenger = add_bus_passenger("manvi")
+print(passenger)
+
+passenger2 = add_bus_passenger("kritika")
+print(passenger2)
