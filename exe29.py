ef4b3a0ed19a1f47926c0268e30b2dbc4f2019df
kritika-2006 <kritika02056@gmail.com>
2026-06-24 16:43:36 +0530

    main

---
+++ exe29.py
@@ -0,0 +1,13 @@
+msg = "cYber sEcuRity"
+print(msg.upper()) # text convert into capital
+print(msg.lower()) # text convert into small letters
+print(msg.title()) # convert the first letter into capital
+
+# checking methods (true/false return karta h)
+file_name = "script.py"
+print(file_name.startswith("sc"))
+print(file_name.endswith(".py"))
+
+a = "  hello  "
+print(a.strip())
+print(a.replace("hello","Hi"))
