6c8a122e87e6e8d2260a457108692fd61a5393e8
kritika-2006 <kritika02056@gmail.com>
2026-06-26 19:32:13 +0530

    main

---
+++ exe39.py/exe.py
@@ -0,0 +1,8 @@
+with open ("activity_logs.txt","a") as file:
+    file.write("Log: System running normally.\n")
+
+with open ("activity_logs.txt","a") as file:
+    file.write("Log: New login detected from user root.\n")
+
+with open ("activity_logs.txt","r") as file:
+    print(file.read())
