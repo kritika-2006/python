7931ab63ae3fb9c38a12e875335f06a83d7caf7c
kritika-2006 <kritika02056@gmail.com>
2026-06-28 23:24:58 +0530

    main

---
+++ exe42.py/oops.py/with attributes.py
@@ -0,0 +1,13 @@
+class Phone:
+    pass
+
+iphone = Phone()
+iphone.brand = "Apple"
+iphone.price = 120000
+
+samsung = Phone()
+samsung.brand = "samsung"
+samsung.price = 95000
+
+print(iphone.brand)
+print(samsung.price)
