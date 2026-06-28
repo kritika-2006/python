9689b98610df92cbf518735e072e38d3b3c9cb24
kritika-2006 <kritika02056@gmail.com>
2026-06-28 23:27:12 +0530

    main

---
+++ exe42.py/oops.py/exe2.py
@@ -0,0 +1,13 @@
+class Phone:
+    pass
+
+iphone = Phone()
+iphone.model = "iphone 15"
+iphone.storage = "128 GB"
+
+samsung = Phone()
+samsung.model = "Galaxy S24"
+samsung.storage = "256 GB"
+
+print(iphone.model)
+print(samsung.model)
