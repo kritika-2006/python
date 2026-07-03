9b8993d5da31666fd916004de6fa2f05bd46bf76
kritika-2006 <kritika02056@gmail.com>
2026-07-03 14:16:43 +0530

    main

---
+++ exe43.py/types inh.py/single.py
@@ -0,0 +1,15 @@
+class Parent:
+    def eat(self):
+        print("This animal eats food")
+
+class child(Parent):
+    def bark(self):
+        print("The dog barks: Woof! Woof!")
+
+d1 = child()
+d1.eat()
+d1.bark()
+
+
+
+        
