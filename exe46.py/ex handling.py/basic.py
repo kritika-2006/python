950430d6746c557ab709129a7a77593d0271bb6d
kritika-2006 <kritika02056@gmail.com>
2026-07-06 23:06:08 +0530

    main

---
+++ exe46.py/ex handling.py/basic.py
@@ -0,0 +1,8 @@
+try:
+    num = int(input("Enter your number:"))
+    print("number:",num)
+
+except:
+    print("invalid! Pls enter valid number not string")
+
+print("System use without any crash ")
