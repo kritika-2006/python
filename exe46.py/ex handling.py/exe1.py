44ab180be18bc66f22c2c904c20b5fae7a06b777
kritika-2006 <kritika02056@gmail.com>
2026-07-06 23:07:47 +0530

    main

---
+++ exe46.py/ex handling.py/exe1.py
@@ -0,0 +1,9 @@
+try:
+    a = int(input("first nuum:"))
+    b = int(input("second num:"))
+    print("Result:",a/b)
+
+except:
+    print("Kuch toh gadbad hui h !")
+
+print("Program abhi jinda h")
